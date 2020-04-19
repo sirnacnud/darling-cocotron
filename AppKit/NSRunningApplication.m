0d335a914328c30ddf7fd88a5007c337ae54fa1b
Lubos Dolezel <lubos@dolezel.info>
2020-04-19 12:17:42 +0200

    Add implementation notes (darlinghq/darling#758)

--- AppKit/NSRunningApplication.m
+++ AppKit/NSRunningApplication.m
@@ -2,6 +2,19 @@
 
 // DUMMY
 
+// Implementation notes:
+// _LSCopyApplicationInformationItem(-2, ...) is used to fetch properties, such as _kLSExecutablePathKey
+// Applications (processes) are referred to by an opaque void* asn (application serial number).
+// ASNs can be compared with _LSCompareASNs().
+//
+// lsd provides notifications when processes change. This is registered via:
+// _LSScheduleNotificationFunction(-2, callback, eventMask, context, CFRunLoopRef, kCFRunLoopCommonModes) and _LSModifyNotification().
+// The properties are updated via KVO.
+//
+// Current application is also observed via LS - _LSGetCurrentApplicationASN().
+// All apps: _LSCopyRunningApplicationArray() - returns an array of ASNs.
+// Running apps: _LSCopyRunningApplicationArray() - ditto.
+
 @interface NSRunningApplication : NSObject
 @end
 
